e18dcae8f785dfff03e0cdf43de7692e3f36a37e
ntd1753 <ntd1753@gmail.com>
2024-04-28 01:49:10 +0700

    feat_migrate

---
+++ database/migrations/2024_04_27_154911_create_pets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ownerId');
+            //$table->foreign('ownerId')->references('id')->on('users');
+            $table->string('name');
+            $table->integer('age');
+            $table->string('color');
+            $table->string('gender');
+            $table->string('avatar')->nullable();
+            $table->string('healthStatus')->nullable();
+            $table->integer('speciesId')->unsigned();
+            //$table->foreign('speciesId')->references('id')->on('species');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pets');
+    }
+};
---
+++ database/migrations/2024_04_27_180531_create_rooms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('RoomNumber');
+            $table->integer('Capacity');
+            $table->enum('Status',['available','unavailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
